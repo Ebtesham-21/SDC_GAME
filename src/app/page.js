c7ea16c1c7d39ed25ef7f763673efd7e6a780592
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-12-08 10:53:17 +0600

    routing fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import Page1 from "./pages/Page1";
-import Page2 from "./pages/Page2";
+
 
 
 
@@ -9,7 +8,7 @@ export default function Home() {
   return (
    <>
   <Page1/>
-    
+
 
 
    </>
