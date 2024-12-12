7c1298f2ad5b7ff0988b06fe5afe8a66fc70b613
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-12-12 16:54:37 +0600

    quiz ui completed

--- src/app/quiz/page.js
+++ src/app/quiz/page.js
@@ -1,59 +1,85 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
 
-export default function Page() {
-  return (
-    <div className="min-h-screen flex flex-col bg-[url('/Assets/Images/BG.png')] bg-cover bg-center">
-      {/* Back Button */}
-      <div className="absolute top-4 left-4">
-        <Link href="./previousPage">
-          <button className="p-2">
-            <img
-              src="/Assets/Images/Back_button.png"
-              alt="Back Button"
-              className="w-12 h-12"
-            />
-          </button>
-        </Link>
-      </div>
+export default function Page(){
+    return(
+        <div className="min-h-screen flex flex-col  bg-[url('/Assets/Images/BG.png')] bg-cover bg-center">
+            <div className="absolute top-4 left-4">
+                <Link href="./qnatwo">
+                <button className="p-2">
+                    <Image src="/Assets/Images/Back_button.png" 
+                    alt="Back Button"
+                    width={50}
+                    height={50}
+                    />
+                </button>
+                </Link>
+            </div>
 
-      {/* Centered Content */}
-      <div className="flex flex-grow justify-center items-center">
-        <div className="grid gap-4 max-w-4xl w-full px-4">
-          {/* First Row - Three Columns */}
-          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-            <div className="bg-white p-4 flex justify-center items-center rounded shadow-md">
-              Option 1
-            </div>
-            <div className="bg-white p-4 flex justify-center items-center rounded shadow-md">
-              Option 2
-            </div>
-            <div className="bg-white p-4 flex justify-center items-center rounded shadow-md">
-              Option 3
-            </div>
-          </div>
+            {/* quiz */}
+            <div className="flex flex-grow justify-center items-center">
+                <div className="grid grid-gap-4 max-w-4xl w-full px-4">
+                    <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+                        <div className=" p-4 flex justify-center items-center ">
+                            <Image   
+                            width={100}
+                            height={100}
+                            alt="home_Button"
+                            src="/Assets/Images/Time_button.png" />
+                        </div>
+                        <div className=" p-4 flex justify-center items-center ">
+                        <Image   
+                            width={600}
+                            height={600}
+                            alt="home_Button"
+                            src="/Assets/Images/Q-n-A-Question-Bar.png" />
+                        </div>
+                        <div className=" p-4 flex justify-center items-center ">
+                        <Image   
+                            width={100}
+                            height={100}
+                            alt="home_Button"
+                            src="/Assets/Images/Score_Button.png" />
+                        </div>
+                    </div>
 
-          {/* Second Row - Two Columns */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div className="bg-white p-4 flex justify-center items-center rounded shadow-md">
-              Option 4
-            </div>
-            <div className="bg-white p-4 flex justify-center items-center rounded shadow-md">
-              Option 5
-            </div>
-          </div>
+              
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 p-4">
+                    <div className=" p-4 flex justify-center items-center">
+                    <Image   
+                            width={600}
+                            height={600}
+                            alt="home_Button"
+                            src="/Assets/Images/Cholera_Q1_OpA.png" />
+                    </div>
+                    <div className=" p-4 flex justify-center items-center">
+                    <Image   
+                            width={600}
+                            height={600}
+                            alt="home_Button"
+                            src="/Assets/Images/Cholera_Q1_OpA.png" />
+                    </div>
+                </div>
 
-          {/* Third Row - Two Columns */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-            <div className="bg-white p-4 flex justify-center items-center rounded shadow-md">
-              Option 6
+                <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                    <div className=" p-4 flex justify-center items-center ">
+                    <Image   
+                            width={600}
+                            height={600}
+                            alt="home_Button"
+                            src="/Assets/Images/Cholera_Q1_OpA.png" />
+                    </div>
+                    <div className=" p-4 flex justify-center items-center">
+                    <Image   
+                            width={600}
+                            height={600}
+                            alt="home_Button"
+                            src="/Assets/Images/Cholera_Q1_OpA.png" />
+                    </div>
+                </div>
             </div>
-            <div className="bg-white p-4 flex justify-center items-center rounded shadow-md">
-              Option 7
-            </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
